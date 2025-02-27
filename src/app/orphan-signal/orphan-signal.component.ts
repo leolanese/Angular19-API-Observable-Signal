0eaa781f216f13b55fa02b4575253ecbb3c41d69
leolanese <laneseleo@gmail.com>
2025-02-27 13:51:57 +0000

    running with orphan with Signals, same service

--- src/app/orphan-signal/orphan-signal.component.ts
+++ src/app/orphan-signal/orphan-signal.component.ts
@@ -11,21 +11,21 @@ import {APIService} from '../api.service';
     <p>AC1) Get all country names and display on the page:</p>
     <button (click)="fetchData('independent?fields=name')">Fetch AC1</button>
 
+    <div *ngIf="selectedCountry() as selectedCountry">
+      <h2>{{ selectedCountry.name.common }}</h2>
+      <img [src]="selectedCountry.flags.png" alt="{{ selectedCountry.name.common }} flag" />
+    </div>
+
     <p>AC2) Select a country and Show the flag:</p>
     <ul>
       @for (item of items(); track item.name.common) {
-        <li (click)="onCountrySelected(item.name.common)">
+        <li (click)="selectCountry(item.name.common)">
           <p>Country name: {{ item.name.common }}</p>
         </li>
       } @empty {
         <li>Loading...</li>
       }
     </ul>
-
-    <div *ngIf="selectedCountry() as selectedCountry">
-      <h2>{{ selectedCountry.name.common }}</h2>
-      <img [src]="selectedCountry.flags.png" alt="{{ selectedCountry.name.common }} flag" />
-    </div>
 
     <p>AC3) Search countries by language:</p>
     <input
@@ -52,8 +52,8 @@ export class OrphanSignalComponent {
     });
   }
 
-  onCountrySelected(countryName: string): void {
-    this.fetchData<any>(`name/${countryName}?fields=name,flags`);
+  selectCountry(countryName: string): void {
+    // this.fetchData<any>(`name/${countryName}?fields=name,flags`);
 
     const url = `${this.apiService.apiUrl}name/${countryName}?fields=name,flags`;
 
